b80ac5c19033c90c4a602f0718817f8ab0b9ee2a
joserolandovelascopena-code <joserolandovelascopena@gmail.com>
2026-08-05 20:35:45 -0600

    Termino diseño, para agregar productos

--- admin/js/eventos.js
+++ admin/js/eventos.js
@@ -16,39 +16,111 @@ logoBtn.addEventListener("click", () => {
 
 const pantallaPrincipal = document.querySelector(".content");
 const pantallaProductos = document.querySelector(".interfaz_productos");
+const pantallaCategorias = document.querySelector(".interfaz_categorias");
 
 const btn_abrirPrincipal = document.querySelector(".inicioPag");
-const btn_abirProductos = document.querySelector(".productosPag");
-const btn_abirCategoria = document.querySelector(".categoriaPag");
+const btn_abrirProductos = document.querySelector(".productosPag");
+const btn_abrirCategoria = document.querySelector(".categoriaPag");
 const btn_abirPedidos = document.querySelector(".pedidosPag");
-const btn_abirInventario = document.querySelector(".inventarioPag");
-const btn_abirClientes = document.querySelector(".clientesPag");
-const btn_abirConfiguracion = document.querySelector(".configuracionPag");
+const btn_abrirInventario = document.querySelector(".inventarioPag");
+const btn_abrirClientes = document.querySelector(".clientesPag");
+const btn_abrirConfiguracion = document.querySelector(".configuracionPag");
 
 btn_abrirPrincipal.addEventListener("click", () => {
   abrirPantalla("principal");
 });
 
-btn_abirProductos.addEventListener("click", () => {
+btn_abrirProductos.addEventListener("click", () => {
   abrirPantalla("productos");
+});
+
+btn_abrirCategoria.addEventListener("click", () => {
+  abrirPantalla("categorias");
 });
 
 function abrirPantalla(pantalla) {
   switch (pantalla) {
     case "productos":
       pantallaPrincipal.classList.remove("show");
+      pantallaCategorias.classList.remove("show");
       pantallaProductos.classList.add("show");
+
       btn_abrirPrincipal.classList.remove("select");
-      btn_abirProductos.classList.add("select");
+      btn_abrirCategoria.classList.remove("select");
+      btn_abrirProductos.classList.add("select");
+      break;
+
+    case "categorias":
+      pantallaPrincipal.classList.remove("show");
+      pantallaProductos.classList.remove("show");
+      pantallaCategorias.classList.add("show");
+
+      btn_abrirPrincipal.classList.remove("select");
+      btn_abrirProductos.classList.remove("select");
+      btn_abrirCategoria.classList.add("select");
 
       break;
     default:
-      pantallaPrincipal.classList.add("show");
       pantallaProductos.classList.remove("show");
-      btn_abirProductos.classList.remove("select");
+      pantallaCategorias.classList.remove("show");
+      pantallaPrincipal.classList.add("show");
+
+      btn_abrirProductos.classList.remove("select");
+      btn_abrirCategoria.classList.remove("select");
       btn_abrirPrincipal.classList.add("select");
+
       break;
   }
 }
 
-abrirPantalla("productos");
+const btnCancelarProduct = document.querySelector(".btnCancelar");
+const btnGuardarProduct = document.querySelector(".btnGuardar");
+
+btnGuardarProduct.onclick = () => {
+  const tituloProduct = document.getElementById("titulo").value.trim();
+  const precioVenta = document.getElementById("precio").value.trim();
+  const marcaProduct = document.getElementById("marca_product").value.trim();
+  const precioOriginal = document.getElementById("precioOriginal").value.trim();
+  const id_producto = document.getElementById("id_product").value.trim();
+  const descripconProduct = document.getElementById("descripcion");
+  validarFormatoInputs(
+    tituloProduct,
+    precioVenta,
+    marcaProduct,
+    precioOriginal,
+    id_producto,
+    descripconProduct,
+  );
+};
+
+let hayError = false;
+
+function validarFormatoInputs(titulo, precio, marca, orginalPrecio, id) {
+  if (titulo.length < 5) {
+    hayError = true;
+    alert("Titulo demasiado corto");
+    return false;
+  }
+
+  if (precio.length < 1) {
+    hayError = true;
+    alert("Precio demasiado corto");
+    return false;
+  }
+
+  if (orginalPrecio.length < 1) {
+    hayError = true;
+    alert("Precio original demasiado corto");
+    return false;
+  }
+
+  if (id.length < 1) {
+    hayError = true;
+    alert("ID demasiado corto");
+    return false;
+  }
+
+  return true;
+}
+
+abrirPantalla("categorias");
